06429e6f5acd5c1895750323d1d39fd420937a6e
LeCGMS <leandrocgms1986@outlook.com>
2024-07-30 22:27:21 -0400

    Ok do script "enviar"

--- js/index.js
+++ js/index.js
@@ -1,27 +1,28 @@
 $(window).ready(() => {
     let stateButton = false
-    setTimeout(() => {
-        alert('Bem vindas à programação, Ana Carolina e Camila.')
-    }, 100)
     $('#mainBtn').on('click', () => {
         if(!stateButton){
-            fetch("https://raw.githubusercontent.com/LeandroCGMS/LeandroCGMS.github.io/main/Familia.txt")
-            .then(async function (response) {
-                if(response.ok){
-                    let text = await response.text()
-                    alert(`Os nomes contidos no arquivo do endereço descrito são:\n\n`+text)
-                    $('#mainTextArea').text(`Aqui também eles são exibidos:\n\n`+text)
-                } else {
-                    alert('Ocorreu um erro ao tentar acessar o endereço web informado -> \n')
-                }
-            }).catch(error => {
-                console.error(error)
-            })
+            // fetch("https://raw.githubusercontent.com/LeandroCGMS/LeandroCGMS.github.io/main/Familia.txt")
+            // .then(async function (response) {
+            //     if(response.ok){
+            //         let text = await response.text()
+            //         alert(`Os nomes contidos no arquivo do endereço descrito são:\n\n`+text)
+            //         $('#mainTextArea').text(`Aqui também eles são exibidos:\n\n`+text)
+            //     } else {
+            //         alert('Ocorreu um erro ao tentar acessar o endereço web informado -> \n')
+            //     }
+            // }).catch(error => {
+            //     console.error(error)
+            // })
         } else {
             alert('Agora, vamos limpar o resultado para tentar novamente.')
             $('#mainTextArea').text("")
         }
         stateButton = !stateButton
         
+    })
+    $('#btnCloseWelcome, #iconCloser').on('click', () =>{
+        console.log('fechou')
+        $('#welcome').hide()
     })
 })
