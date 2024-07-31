7745c5249f447acef45f0fe04e4fee938ddab83f
LeCGMS <leandrocgms1986@outlook.com>
2024-07-31 10:37:35 -0400

    Ok do script "enviar"

--- js/index.js
+++ js/index.js
@@ -1,39 +1,39 @@
 $(window).ready(() => {
+    $('#divResult').hide()
     let stateButton = false
     $('#mainBtn').on('click', async () => {
         const urlToGet = $('#mainTextArea').val()
-        if (!stateButton) {
-            // fetch("https://raw.githubusercontent.com/LeandroCGMS/LeandroCGMS.github.io/main/Familia.txt")
-            // .then(async function (response) {
-            //     if(response.ok){
-            //         let text = await response.text()
-            //         alert(`Os nomes contidos no arquivo do endereço descrito são:\n\n`+text)
-            //         $('#mainTextArea').text(`Aqui também eles são exibidos:\n\n`+text)
-            //     } else {
-            //         alert('Ocorreu um erro ao tentar acessar o endereço web informado -> \n')
-            //     }
-            // }).catch(error => {
-            //     console.error(error)
-            // })
-            try {
-                result = await fetch(urlToGet)
-                const contentType = response.headers.get('content-type')
-                if (contentType.includes('application/json')) {
-                    return response.json();
-                } else if (contentType.includes('text/')) {
-                    return response.text();
-                } else if (contentType.includes('image/')) {
-                    return response.blob();
-                } else {
-                    
-                }
-            } catch (error) {
+        // fetch("https://raw.githubusercontent.com/LeandroCGMS/LeandroCGMS.github.io/main/Familia.txt")
+        // .then(async function (response) {
+        //     if(response.ok){
+        //         let text = await response.text()
+        //         alert(`Os nomes contidos no arquivo do endereço descrito são:\n\n`+text)
+        //         $('#mainTextArea').text(`Aqui também eles são exibidos:\n\n`+text)
+        //     } else {
+        //         alert('Ocorreu um erro ao tentar acessar o endereço web informado -> \n')
+        //     }
+        // }).catch(error => {
+        //     console.error(error)
+        // })
+        try {
+            result = await fetch(urlToGet)
+            console.log(`result -> `, result, await result.text())
+            const contentType = response.headers.get('content-type')
+            console.log(`contentType -> `, contentType)
+            if (contentType.includes('application/json') || contentType.includes('text/')) {
+                const text = await result.text()
+                $('#divResult').show()
+                $('#divTextResult').text(await result.text())
+                console.log(`texto -> ${text}`)
+            } else if (contentType.includes('image/')) {
+                return response.blob();
+            } else {
 
             }
-        } else {
-            alert('Agora, vamos limpar o resultado para tentar novamente.')
-            $('#mainTextArea').text("")
+        } catch (error) {
+
         }
+
         stateButton = !stateButton
 
     })
